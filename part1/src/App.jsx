1fbab457cedadfb0cce6c7e8fd90ba48e4e8187f
Brais Gonzalez <braisgmendu@gmail.com>
2024-12-25 18:39:44 +0100

    hechos ejercicio 1.1 y 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,7 @@
-const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4 },
-    { name: 'Maya', age: 10 },
-  ]
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-    </div>
+    <div>{counter}</div>
   )
 }
 
