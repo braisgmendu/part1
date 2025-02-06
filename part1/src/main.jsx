e057474396b64bad5aea0c5ff8c4036a839dbaac
Brais Gonzalez <braisgmendu@gmail.com>
2025-02-06 22:36:49 +0100

    acabado unicafe part1

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -2,15 +2,8 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
 
-const refresh = () => {
   ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
+    <App />
   )
-}   
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
+   
--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,61 +1,83 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
+import { useState } from 'react'
+const StatisticsLine = (props) =>{
+  return(
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
-
-const Part = (props) => {
+const Statistics = (props) => {
+  if (props.all === 0){
+    return <p>No feedback given</p>
+  }
+  else{
+    return(
+      <div>
+        <h2>Statistics</h2>
+        <table>
+          <tbody>
+            <StatisticsLine text="good" value={props.good} />
+            <StatisticsLine text="neutral" value={props.neutral} />
+            <StatisticsLine text="bad" value={props.bad} />
+            <StatisticsLine text="all" value={props.all} />
+            <StatisticsLine text="average:" value={props.all > 0 ? (props.average/props.all).toFixed(2): 0} />
+            <StatisticsLine text="positive:" value={props.all > 0 ? ((props.good/props.all)*100).toFixed(2): 0} />
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+const Button = (props) => {
   return (
-    <p>{props.name} {props.ex}</p>
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part name={props.part1} ex={props.exercises1} />
-      <Part name={props.part2} ex={props.exercises2} />
-      <Part name={props.part3} ex={props.exercises3} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const totalExercises = props.ex1 + props.ex2 + props.ex3;
-  return (
-    <p>Number of exercises {totalExercises}</p>
-  )
-}
-
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+
+  const handleGoodClick = () => {
+    const updateGood = good + 1;
+    setGood(updateGood)
+    setAll(updateGood + neutral + bad)
+    setAverage(average + 1)
+  }
+  const handleNeutralClick = () => {
+    const updateNeutral = neutral + 1
+    setNeutral(updateNeutral)
+    setAll(updateNeutral + good + bad)
+    setAverage(average + 0)
+
+  }
+  const handleBadClick = () => {
+    const updateBad = bad + 1
+    setBad(updateBad)
+    setAll(updateBad + good + neutral)
+    setAverage(average -1 )
+
   }
   return (
     <div>
-     <Header course={course.name} />
-     <Content part1={course.parts[0].name} exercises1={course.parts[0].exercises} 
-     part2={course.parts[1].name} exercises2={course.parts[1].exercises} part3={course.parts[2].name} 
-     exercises3={course.parts[2].exercises} />
-     <Total ex1={course.parts[0].exercises} ex2={course.parts[1].exercises} ex3={course.parts[2].exercises}  />
+      <div>
+        <h2>give feedback</h2>
+      </div>
+      <div>
+        <Button onClick={handleGoodClick} text="good" />
+        <Button onClick={handleNeutralClick} text="neutral" />
+        <Button onClick={handleBadClick} text="bad" />
+       
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average}/>
     </div>
   )
 }
+
 export default App
